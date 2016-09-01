fbf66b2de4cb91d2a4c5353222112c6a77949dcb
cevau <christoph_voigt@yahoo.de>
2016-09-01 09:45:04 +0200

    Now with Context Menue

--- app/src/main/java/com/example/cevau/amando/gui/Startseite.java
+++ app/src/main/java/com/example/cevau/amando/gui/Startseite.java
@@ -3,7 +3,10 @@ package com.example.cevau.amando.gui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.cevau.amando.R;
 
@@ -11,16 +14,69 @@ public class Startseite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+        //Startseite festlegen
         setContentView(R.layout.startseite);
+
+        //Kontextmenue hinzufügen für einzelne View = button starte geokontakte
+        registerForContextMenu(findViewById(R.id.sf_starte_geokontakte));
+
+        super.onCreate(savedInstanceState);
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        /* just a comment */
+        /* just a debug demo */
         Log.d("tag","CV Message");
+
+        //include the option-menu
         getMenuInflater().inflate(R.menu.hauptmenue, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opt_einstellungenBearbeiten:
+                Log.i("menu","opt_einstellungenBearbeiten");
+                return false;
+            case R.id.opt_hilfe:
+                Log.i("menu","opt_hilfe");
+                return false;
+            case R.id.opt_amandoBeenden:
+                Log.i("menu","opt_amandoBeenden");
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        //include context menu for each button which should have a context menu:
+        switch (v.getId()){
+            case R.id.sf_starte_geokontakte:
+                getMenuInflater().inflate(R.menu.startseite_contextmenue,menu);
+                return;
+        }
+
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.opt_geokontakt_verwalten_hilfe:
+                //todo rufe hilfeseite auf...
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 }
